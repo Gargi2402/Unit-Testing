8add61bca85c54511072f96b78fbfdaee8c6070a
Gargi2402 <gargidave2402@gmail.com>
2025-02-13 13:04:27 -0500

    Added Isosceles Triangle Tests

--- TriangleTest/Class1.cs
+++ TriangleTest/Class1.cs
@@ -23,5 +23,47 @@ namespace TriangleTest
             // Assert
             Assert.AreEqual("Equilateral triangle", result);
         }
+
+        //Test case 1: First two sides are equal
+        [Test]
+        public void AnalyzeTriangle_IsoscelesTriangle_Case1()
+        {
+            // Arrange
+            int side1 = 15, side2 = 15, side3 = 24;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            Assert.AreEqual("Isosceles triangle", result);
+        }
+
+        //Test Case 2: Last two sides are equal
+        [Test]
+        public void AnalyzeTriangle_IsoscelesTriangle_Case2()
+        {
+            // Arrange
+            int side1 = 24, side2 = 66, side3 = 66;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            Assert.AreEqual("Isosceles triangle", result);
+        }
+
+        //Test Case 3: First and last sides are equal
+        [Test]
+        public void AnalyzeTriangle_IsoscelesTriangle_Case3()
+        {
+            // Arrange
+            int side1 = 10, side2 = 5, side3 = 10;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            Assert.AreEqual("Isosceles triangle", result);
+        }
     }
 }
